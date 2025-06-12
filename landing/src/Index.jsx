ea493c693efd62f61192369388a24bc63864dd04
oo1991 <87750941+oo1991@users.noreply.github.com>
2025-06-12 11:26:14 +0300

    fix favicon icon

--- landing/src/Index.jsx
+++ landing/src/Index.jsx
@@ -33,7 +33,7 @@ export default function Index() {
     <Html lang="en">
       <Head>
         <meta charSet="UTF-8" />
-        <Link rel="icon" type="image/svg+xml" href="/favicon.ico" />
+        <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Link rel="preconnect" href="https://fonts.googleapis.com" />
         <Link
